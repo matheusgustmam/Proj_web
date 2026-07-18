7301d9bebb63c8fd946f7ae07d460cb6fde0ae09
MatheusGTS <matheusgustmam@gmail.com>
2026-07-18 20:53:52 -0300

    pag principal com os template

---
+++ src/view/templates/template-menu.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="agrinovajr</a>"
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+
+    </div>
+
+</nav>
