12bec28950a18f52144c25dd5f8d67f777fe5cf9
MatheusGTS <matheusgustmam@gmail.com>
2026-07-18 23:21:04 -0300

    index com rotas para testes

--- public/index.php
+++ public/index.php
@@ -9,6 +9,12 @@ define('BASE_URL','Proj_web');
 $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r){
 
     $r->get('/', 'ClienteController@index');
+    $r->get('/clientes', 'ClienteController@listar');
+    $r->get('/clientes/novo', 'ClienteController@novo');
+    $r->get('/clientes/{id}/editar', 'ClienteController@editar');
+    $r->get('/clientes/{id}', 'ClienteController@buscar');
+    $r->post('/clientes/cadastrar', 'ClienteController@cadastrar');
+    $r->post('/clientes/{id}/remover', 'ClienteController@remover');
 
 });
 
