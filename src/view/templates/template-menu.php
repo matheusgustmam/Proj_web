13781eea2747e6be9e4fe043de55a1dba3dfac61
MatheusGTS <matheusgustmam@gmail.com>
2026-07-27 14:42:39 -0300

    view de menu

--- src/view/templates/template-menu.php
+++ src/view/templates/template-menu.php
@@ -1,12 +1,16 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="agrinovajr</a>"
+        <a class="navbar-brand" href="<?= BASE_URL . "/" ?>">AgrinovaJR</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
     </div>
     <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
 
+            </li>
+        </ul>
     </div>
 
 </nav>
